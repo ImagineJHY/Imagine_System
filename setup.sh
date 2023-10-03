adf4e994c48f450ff829ab08831299936ac5f91e
Imagine <imaginejhy@163.com>
2023-10-03 15:53:31 +0800

    update CMakeLists.txt

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# current_dir=$(pwd)
-export PATH=$PATH:${CMAKE_CURRENT_SOURCE_DIR}/worker/Imagine_Muduo/include
-export PATH=$PATH:${CMAKE_CURRENT_SOURCE_DIR}/worker/Imagine_ZooKeeper/include
-export PATH=$PATH:${CMAKE_CURRENT_SOURCE_DIR}/worker/Imagine_Rpc/include
-export PATH=$PATH:${CMAKE_CURRENT_SOURCE_DIR}/worker/Imagine_MapReduce/include
+current_dir=$(pwd)
+export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_Muduo/include
+export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_ZooKeeper/include
+export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_Rpc/include
+export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_MapReduce/include
 echo PATH is $PATH
