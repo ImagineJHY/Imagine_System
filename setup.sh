5307f97abd9c4d5809bf89775540b38ee5281ad5
Imagine <imaginejhy@163.com>
2023-10-03 13:58:45 +0800

    add setup.sh file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+current_dir=$(pwd)
+export PATH=$PATH:${current_dir}/worker/Imagine_Muduo/include
+export PATH=$PATH:${current_dir}/worker/Imagine_ZooKeeper/include
+export PATH=$PATH:${current_dir}/worker/Imagine_Rpc/include
+export PATH=$PATH:${current_dir}/worker/Imagine_MapReduce/include
