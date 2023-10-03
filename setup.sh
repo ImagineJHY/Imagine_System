b5cd636ad70f33b90637433b7bc32df8ffa778eb
Imagine <imaginejhy@163.com>
2023-10-03 14:42:43 +0800

    update Imagine_Muduo branch and commit

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ export PATH=$PATH:${current_dir}/worker/Imagine_Muduo/include
 export PATH=$PATH:${current_dir}/worker/Imagine_ZooKeeper/include
 export PATH=$PATH:${current_dir}/worker/Imagine_Rpc/include
 export PATH=$PATH:${current_dir}/worker/Imagine_MapReduce/include
+echo PATH is $PATH
