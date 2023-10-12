66557f9149d700a0827b86143499d5666babe339
ImagineJHY <40879017+ImagineJHY@users.noreply.github.com>
2023-10-12 17:42:37 +0800

    Revert "Imagine/config submodule"

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-current_dir=$(pwd)
-export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_Muduo/include
-export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_ZooKeeper/include
-export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_Rpc/include
-export PATH=$PATH:${current_dir}/thirdparty/Imagine_System/worker/Imagine_MapReduce/include
-echo PATH is $PATH
